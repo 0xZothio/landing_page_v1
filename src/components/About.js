fb14dcd89d52855702d21ade22de32e92fbe9eb1
karthikeya <karthikeyakotegar@gmail.com>
2023-01-12 18:19:29 +0530

    about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <div className="text-[100px] text-center font-bold">About Us</div>
+
+      <div className="mt-6">
+        <p className="sm:w-2/3 h-full text-[24px] text-center mx-auto ">
+          Zoth is a B2B2C marketplace for tokenized assets that helps retail
+          investors diversify their portfolios through investing in high-quality
+          alternative assets such as Real Estate, Art, and Private Equity, just
+          like Stocks. Zoth offers comprehensively vetted properties for
+          fractional ownership, provides asset owners with quick capital and has
+          global investor reach. Investors can Buy, Sell, and Earn a rental
+          yield on Zoth.
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
