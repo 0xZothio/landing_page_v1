6ba1784f48d57ac69e3eb3a9203c23b4f7937649
karthikeya <karthikeyakotegar@gmail.com>
2023-01-14 11:31:35 +0530

    club

---
+++ src/components/Features.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Features() {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-[80px] text-center sm:text-[100px] font-bold">Welcome to the</div>
+        <img src="/club.png" alt="Zoth Club" />
+      </div>
+    </div>
+  );
+}
+
+export default Features;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import About from "../components/About";
+import Features from "../components/Features";
 import Testimonials from "../components/Testimonials";
 
 function LandingPage() {
   return (
     <>
+      {/* Features */}
+      <Features />
+
       {/* About Us */}
       <About />
 
