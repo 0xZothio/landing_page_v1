5b0eed752b3b2c50b2275cbc2d46c347b49e7a99
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-01-18 11:28:40 -0800

    Prealoader

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import About from "../components/About";
 import Features from "../components/Features";
 import Testimonials from "../components/Testimonials";
@@ -7,13 +7,21 @@ import NewsLetter from "../components/newsletter";
 import Footer from "../components/Footer";
 import Assets from "../components/Assets";
 import HowToInvest from "../components/HowToInvest";
+
 function LandingPage() {
+  const [isLoading,setIsLoading]=useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    },4000)
+  })
   return (
     <>
+    {isLoading?<div id="loading"></div>:null}
       {/* Hero */}
       <Hero />
 
-      <HowToInvest/>
+      <HowToInvest />
       {/* Assets */}
       <Assets />
 
