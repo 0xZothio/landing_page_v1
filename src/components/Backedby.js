ea1491312639ef644d214507873e958d1f9ac61a
srinivastejavt26 <thirusrinivasa@quickwork.co>
2023-01-20 20:29:12 +0530

    Update Backedby.js

--- src/components/Backedby.js
+++ src/components/Backedby.js
@@ -2,7 +2,7 @@ import logo from "../assets/images/newlogo.png";
 const Backedby = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="mx-auto mb-4 lg:max-w-xl sm:text-center">
+      {/* <div className="mx-auto mb-4 lg:max-w-xl sm:text-center">
         <p className=" text-3xl tracking-tight sm:text-4xl tect-indigo-300">
           Backed by
         </p>
@@ -11,7 +11,7 @@ const Backedby = () => {
         <p className=" text-3xl font-bold tracking-tight sm:text-4xl">
           The World's Best
         </p>
-      </div>
+      </div> */}
       <div className="grid mx-auto lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-4 bg-gray-600 py-10 rounded-3xl shadow text-white">
         <div className="flex flex-col items-center">
           <img
