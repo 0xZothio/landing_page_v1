413eb6e4c0f70248a7fd2e85ab9fea4a5cf4d394
karthikeya <karthikeyakotegar@gmail.com>
2023-01-25 12:43:44 +0530

    features mobile

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,6 +4,7 @@ import { render } from "react-dom";
 import { motion } from "framer-motion";
 import { fadeIn, slideIn, featureIn, staggerContainer } from "../utils/motion";
 import "../styles/styles.css";
+import { featureData } from "../data/featuresData";
 function Features() {
   const ref = useRef(null);
 
@@ -21,7 +22,7 @@ function Features() {
       description:
         "On the blockchain, transactions are transparent and verifiable. Zoth provides liquidity for on chain assets through its unique real-time market based exit mechanism.",
     },
-    
+
     {
       video: "/features/loyalty.mp4",
       topic: "Governance model for, of and by the users ",
@@ -105,7 +106,7 @@ function Features() {
       {/* feature 1*/}
 
       <div
-        className="absolute bg-transparent w-full h-screen overflow-y-scroll z-10 scrollbar-hide mt-40"
+        className="sm:absolute bg-transparent w-full h-screen overflow-y-scroll z-10 scrollbar-hide mt-40 hidden"
         ref={ref}
         id="section-1"
       >
@@ -115,7 +116,7 @@ function Features() {
         <div className="h-screen w-full"></div>
       </div>
 
-      <div className="grid grid-row-4 sm:grid-cols-4 gap-2 justify-items-center content-center p-8">
+      <div className="sm:grid grid-row-4 sm:grid-cols-4 gap-2 justify-items-center content-center p-8 hidden">
         <div className="relative col-span-2 w-[300px]">
           <div className="w-[300px]">
             <img src="/frame.png" alt="frame" />
@@ -342,48 +343,37 @@ function Features() {
         )}
       </div>
 
+      {/* mobile */}
       {/* feature 2*/}
-      {/* <div className="grid grid-row-4 sm:grid-cols-4 gap-2 justify-items-center content-center p-8 sm:mt-[500px]">
-        <div className="relative col-span-2 w-[300px]">
-          <div className="w-[300px]">
-            <img src="/frame.png" alt="frame" />
+      {featureData.map((feature) => (
+        <div className="grid grid-row-4 sm:grid-cols-4 gap-2 justify-items-center content-center p-8 sm:mt-[500px] sm:hidden">
+          <div className="relative col-span-2 w-[300px]">
+            <div className="w-[300px]">
+              <img src="/frame.png" alt="frame" />
+            </div>
+
+            <div className="absolute top-5 object-cover ">
+              {/* <img src="eth.png" alt="feature" className="w-full h-full" /> */}
+              <video
+                src={feature.video}
+                autoplay="{true}"
+                loop
+                muted
+                className="w-[300px] h-[530px] rounded-[55px] opacity-80"
+              ></video>
+            </div>
           </div>
 
-          
-          <motion.div
-            variants={featureIn("left", "tween", 0.8, 0.3)}
-            initial="hidden"
-            whileInView="show"
-            viewport={{ once: false, amount: 0.8 }}
-            className="absolute top-5 object-cover "
-          > */}
-      {/* <img src="eth.png" alt="feature" className="w-full h-full" /> */}
-      {/* <video
-              src="/features/reward.mp4"
-              autoplay="{true}"
-              loop
-              muted
-              className="w-[300px] h-[530px] rounded-[55px] opacity-80"
-            ></video>
-          </motion.div>
-        </div> */}
-
-      {/* <motion.div
-          variants={slideIn("up", "tween", 0.5, 1)}
-          initial="hidden"
-          whileInView="show"
-          className="col-span-2 self-center mt-2 p-8 "
-        >
-          <div className="sm:text-6xl text-4xl font-extrabold font-roobert mb-4">
-            Exclusive rewards for investing in your assets
+          <div className="col-span-2 self-center mt-2 p-8 ">
+            <div className="sm:text-6xl text-3xl font-extrabold font-roobert mb-4">
+              {feature.topic}
+            </div>
+            <div className="text-lg leading-loose">
+              {feature.description}
+            </div>
           </div>
-          <div className="text-lg leading-loose">
-            every time you invest on ZOTH, you receive ZOTH coins. you can use
-            these to win exclusive rewards or get special access to curated
-            products and experiences.
-          </div>
-        </motion.div>
-      </div> */}
+        </div>
+      ))}
 
       {/* feature 3*/}
       {/* <div className="grid grid-row-4 sm:grid-cols-4 gap-2 justify-items-center content-center p-8 sm:mt-[500px]">
