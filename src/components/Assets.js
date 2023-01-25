88ef36284417cb91ba6e22c0c10e763589c9f3fb
karthikeya <karthikeyakotegar@gmail.com>
2023-01-25 18:05:12 +0530

    welocme comp

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -108,42 +108,7 @@ function Assets({ isHidden }) {
 
         {/* Text */}
 
-        <div className="mt-20 sm:mt-4">
-          <h3 id="resizing-h3">
-            <span>
-              <div className="stage">
-                <div className="cubespinner text-center">
-                  <div className="face1">
-                    <div className="text-[40px] sm:text-[55px] text-center">
-                      Democratising{" "}
-                      <span className="text-[#F3C74E]">Investment</span>
-                    </div>
-                  </div>
-                  <div className="face2">
-                    <div className="text-[40px] sm:text-[55px] text-center">
-                      Democratising{" "}
-                      <span className="text-[#F3C74E]">Wealth Creation</span>
-                    </div>
-                  </div>
-                  <div className="face3">
-                    <div className="text-[40px] sm:text-[55px] text-center">
-                      Democratising{" "}
-                      <span className="text-[#F3C74E]">Future of Finance</span>
-                    </div>
-                  </div>
-                  <div className="face4">
-                    <div className="text-[40px] sm:text-[55px] text-center">
-                      Democratising{" "}
-                      <span className="text-[#F3C74E]">
-                        Access to Top Assets
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </span>
-          </h3>
-        </div>
+        
     </div>
   );
 }
