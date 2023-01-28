9bb3891dc3439cda479f9de450155ea57cc63a2c
karthikeya <karthikeyakotegar@gmail.com>
2023-01-28 14:44:39 +0530

    oferrings

--- src/components/Institutions.js
+++ src/components/Institutions.js
@@ -8,8 +8,9 @@ function Institutions() {
   return (
     <>
       {/* <div className="px-6 pt-6 lg:px-8"> */}
-        <Navbar setIsVisible={setIsVisible} />
+      {/* <Navbar setIsVisible={setIsVisible} /> */}
       {/* </div> */}
+
       <div
         id="institutions"
         className="bg-black text-white w-full flex flex-col items-center justify-center h-full mt-[80px] py-10"
@@ -20,65 +21,98 @@ function Institutions() {
           whileInView="show"
           className="text-[55px] text-center sm:text-[80px] font-codec font-extrabold"
         >
-          Zoth for{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
-            Institutions
-          </span>
+            Zoth's{" "}
+          </span>{" "}
+          Offerings
         </motion.div>
-        <div class="paragraph--type--card-row mt-10">
-          <div class="card">
-            <img
-              src="/institution/tokenization.png"
-              alt="Card image"
-              class="card-image p-6"
-            ></img>
-            <div class="card-content">
-              <h3 class="card-title">TAAS</h3>
-              <p class="card-text">
-                Zoth Tokenization as a Service (TAAS) platform provides tokens
-                that are compliant across several jurisdictions globally.Zoth
-                has third party partners that audit the tokenization process
-                regularly.
-              </p>
-              <a href="#" class="btns font-semibold">
-                Coming Soon
-              </a>
+
+        <div className="flex flex-col sm:flex-row">
+          {/* cards 1 */}
+          <div class="flex justify-center my-2 mx-4 hover:scale-105 transition duration-700 ease-in-out ">
+            <div class="rounded-lg shadow-lg bg-black border border-slate-500	 max-w-sm">
+              <div>
+                <img
+                  class="rounded-t-lg px-4 py-8"
+                  src="institution/tokenization.png"
+                  alt=""
+                />
+              </div>
+              <div class="p-6">
+                <h5 class="text-white text-3xl font-medium mb-2 font-codec">
+                  Tokenization
+                </h5>
+                <p class="text-gray-500 text-base mb-8 font-roobert">
+                  Zoth Tokenization as a Service (TAAS) platform provides tokens
+                  that are compliant across several jurisdictions globally.Zoth
+                  has third party partners that audit the tokenization process
+                  regularly.
+                </p>
+                <button
+                  type="button"
+                  class="w-full inline-block px-6 py-2.5 bg-black text-[#F3C74E] font-semibold text-sm font-roobert leading-tight rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out border-t-2 border-slate-700	"
+                >
+                  Know More
+                </button>
+              </div>
             </div>
           </div>
-          <div class="card">
-            <img
-              src="/institution/custody.png"
-              alt="Card image"
-              class="card-image p-6"
-            ></img>
-            <div class="card-content">
-              <h3 class="card-title">Compliant</h3>
-              <p class="card-text">
-                Zoth TAAS platform provides tokens that are compliant across
-                several jurisdictions globally.Zoth has third party partners
-                that audit the tokenization process regularly.
-              </p>
-              <a href="#" class="btns font-semibold">
-                Coming Soon
-              </a>
+
+          {/* cards 2 */}
+          <div class="flex justify-center my-2 mx-4 hover:scale-105 transition duration-700 ease-in-out">
+            <div class="rounded-lg shadow-lg bg-black border border-slate-500	 max-w-sm">
+              <div>
+                <img
+                  class="rounded-t-lg px-4 py-8"
+                  src="institution/custody.png"
+                  alt=""
+                />
+              </div>
+              <div class="p-6">
+                <h5 class="text-white text-3xl font-medium mb-2 font-codec">
+                  Custody
+                </h5>
+                <p class="text-gray-500 text-base mb-8 font-roobert">
+                  Zoth TAAS platform provides tokens that are compliant across
+                  several jurisdictions globally.Zoth has third party partners
+                  that audit the tokenization process regularly.
+                </p>
+                <button
+                  type="button"
+                  class="w-full inline-block px-6 py-2.5 bg-black text-[#F3C74E] font-semibold text-sm font-roobert leading-tight rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out border-t-2 border-slate-700	"
+                >
+                  Know More
+                </button>
+              </div>
             </div>
           </div>
-          <div class="card">
-            <img
-              src="/institution/xchange.png"
-              alt="Card image"
-              class="card-image p-6"
-            ></img>
-            <div class="card-content">
-              <h3 class="card-title">Fund as a Service</h3>
-              <p class="card-text">
-                Zoth allows institutions and funds to invest in the tokenized
-                asset pools and liquidity pools. Zoth curates these assets from
-                quality locations that provide competitive yield.
-              </p>
-              <a href="#" class="btns font-semibold">
-                Coming Soon
-              </a>
+
+          {/* cards 3 */}
+          <div class="flex justify-center my-4 mx-4 hover:scale-105 transition duration-700 ease-in-out">
+            <div class="rounded-lg shadow-lg bg-black border border-slate-500	 max-w-sm">
+              <div>
+                <img
+                  class="rounded-t-lg px-4 py-8"
+                  src="institution/xchange.png"
+                  alt=""
+                />
+              </div>
+              <div class="p-6">
+                <h5 class="text-white text-3xl font-medium mb-2 font-codec">
+                  Exchange
+                </h5>
+                <p class="text-gray-500 text-base mb-8 font-roobert">
+                  Zoth allows institutions and funds to invest in the tokenized
+                  asset pools and liquidity pools. Zoth curates these assets
+                  from quality locations that provide competitive yield.
+                </p>
+                <button
+                  type="button"
+                  class="w-full inline-block px-6 py-2.5 bg-black text-[#F3C74E] font-semibold text-sm font-roobert leading-tight rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out border-t-2 border-slate-700	"
+                >
+                  Know More
+                </button>
+              </div>
             </div>
           </div>
         </div>
