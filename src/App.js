f56ef9a795ac7b213e7cd759a8739d51be4c264f
srinivastejavt26 <thirusrinivasa@quickwork.co>
2023-02-03 13:25:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import Institutions from "./components/Institutions";
-
+import ReactGA from 'react-ga';
+import { useEffect } from "react";
+  const TRACKING_ID = "UA-256346841-1"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="bg-black text-white w-full h-full overflow-hidden">
       <Router>
